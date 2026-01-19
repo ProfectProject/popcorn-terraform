84d7bd7b6819d291993738e49a9f45b6e5187b84
hongjunpyo <ejhjjp11@naver.com>
2026-01-19 12:55:25 +0900

    [style]: vpc 모듈 포맷 정리

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -20,10 +20,10 @@ resource "aws_internet_gateway" "this" {
 }
 
 resource "aws_subnet" "public" {
-  for_each          = local.public_subnets
-  vpc_id            = aws_vpc.this.id
-  availability_zone = each.value.az
-  cidr_block        = each.value.cidr
+  for_each                = local.public_subnets
+  vpc_id                  = aws_vpc.this.id
+  availability_zone       = each.value.az
+  cidr_block              = each.value.cidr
   map_public_ip_on_launch = true
 
   tags = merge(var.tags, {
